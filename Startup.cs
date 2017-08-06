2e1c71797d4e08a80d90a725812be4db15333cb6
Andrew Parks <parksdrew01@gmail.com>
2017-08-06 15:00:55 -0700

    add photo and static config to startup file

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace Address
 
          public void Configure(IApplicationBuilder app)
          {
+            app.UseStaticFiles();
              app.UseMvc(routes =>
              {
                  routes.MapRoute(
