6015fc7c9935b02d1487f644e310ad830ad2389a
Andrew Parks <parksdrew01@gmail.com>
2017-08-06 18:26:27 -0700

    add form with html

--- Controllers/HomeControllers.cs
+++ Controllers/HomeControllers.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using Address.Models;
 
-namespace Address.Controllers
+namespace LetterVariables.Models
 {
     public class HomeController : Controller
-    {
-[Produces("text/html")]
-[Route("/")]
-public ActionResult Interface()
 {
-  return View();
-}
-  }
+  [Produces("text/html")]
+  [Route("/favorite_photos")]
+  [Route("/form")]
+    public ActionResult Form()
+      {
+        return View();
+      }
+  [Produces("text/html")]
+  [Route("/greeting_card")]
+  public ActionResult GreetingCard()
+        {
+          LetterVariable myLetterVariable = new LetterVariable();
+          myLetterVariable.SetRecipient(Request.Query["recipient"]);
+          myLetterVariable.SetSender(Request.Query["sender"]);
+          return View("Hello", myLetterVariable);
+        }
+    }
+
 }
